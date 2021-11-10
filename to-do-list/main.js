40acc8dc852d96e2ec93f4ef43d1fcd9b3560093
Omima Khaled <85596970+omima618@users.noreply.github.com>
2021-11-10 08:28:19 +0200

    add delete all button and done feature

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -1,6 +1,7 @@
 let inp = document.querySelector(".task-name");
 let addBtn = document.querySelector(".add");
 let container = document.querySelector(".container");
+let delAll = document.querySelector(".del-all");
 let tasksArr = [];
 if (window.localStorage.getItem("tasks")) {
     tasksArr = JSON.parse(window.localStorage.getItem("tasks"));
@@ -12,16 +13,41 @@ addBtn.onclick = function () {
         inp.value = "";
     }
 };
+delAll.addEventListener("click", function () {
+    container.innerHTML = "";
+    tasksArr = [];
+    window.localStorage.removeItem("tasks");
+});
 container.addEventListener("click", (e) => {
     if (e.target.classList.contains("del")) {
         removeFromLocalStorage(e.target.parentElement.getAttribute("data-id"));
         e.target.parentElement.remove();
     }
+    if (e.target.classList.contains("task")) {
+        e.target.classList.toggle("done");
+        doneStatus(e.target.getAttribute("data-id"));
+    }
+    if (e.target.classList.contains("del-all")) {
+        container.innerHTML = "";
+        window.localStorage.clear();
+    }
 });
+function doneStatus(taskId) {
+    for (let i = 0; i < tasksArr.length; i++) {
+        if (tasksArr[i].id == taskId) {
+            tasksArr[i].done === false
+                ? (tasksArr[i].done = true)
+                : (tasksArr[i].done = false);
+        }
+    }
+    addDataToLocalStorage(tasksArr);
+}
+
 function addTasksToArr(taskText) {
     const task = {
         id: Date.now(),
         title: taskText,
+        done: false,
     };
     tasksArr.push(task);
     addElementsToPage(tasksArr);
@@ -32,6 +58,9 @@ function addElementsToPage(tasksArr) {
     tasksArr.forEach((task) => {
         let taskDiv = document.createElement("div");
         taskDiv.className = "task";
+        if (task.done === true) {
+            taskDiv.className = "task done";
+        }
         taskDiv.setAttribute("data-id", task.id);
         taskDiv.appendChild(document.createTextNode(task.title));
         let delBtn = document.createElement("button");
